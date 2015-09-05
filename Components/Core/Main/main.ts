5c0461e6553d0903c82ad57d7f4db49eec50ccd2
Jean2bob <francois.guezengar@hotmail.fr>
2015-09-05 13:23:41 +0200

    Index

---
+++ Components/Core/Main/main.ts
@@ -0,0 +1,15 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector : 'main'
+})
+
+@View({
+    templateUrl: 'Components/Core/Main/main_template.html'
+})
+
+class MainComponent{
+
+}
+
+bootstrap(MainComponent);
