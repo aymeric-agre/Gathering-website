783276cc635dc278ca1a4ad1955165417bd151fa
Aymeric <aymeric.agre@gmail.com>
2015-09-05 15:22:34 +0200

    correc server

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var express = require('express'),
+	
 	app = express();
 
-app.use(express.static('public'));
+/**
+ * Rendering engine
+ */
+// app.engine('html', cons.swig);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/Public/Views');
+
+/**
+ * Definition of middlewares
+ */
+// app.use(bodyParser());
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', function(req, res){
+	res.end()
+})
 
 var server = app.listen(3000, function(){
 	var host = server.address().address;
