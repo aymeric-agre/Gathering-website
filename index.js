bb779e4e9639c490c049cbd90a9f282e0bc6112e
Aymeric <aymeric.agre@gmail.com>
2015-09-05 12:47:24 +0200

    correction app.configure (express 3)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app);
+	app = express();
 
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static('public'));
 
-server.listen(80);
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('Server app at http://%s:%s', host, port);
+})
