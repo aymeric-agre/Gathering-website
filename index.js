b978511205289dfaaae2150e2ecf8cbd84ce0a84
Jean2bob <francois.guezengar@hotmail.fr>
2015-09-05 15:19:44 +0200

    Components

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express'),
 	app = express();
 
 app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+	res.render('index.html');
+});
 
 var server = app.listen(3000, function(){
 	var host = server.address().address;
